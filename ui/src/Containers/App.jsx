7f578aba5c0d66a3f821d5820b69e73a0f2c4025
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-06 15:01:27 -0300

    sidebar

---
+++ ui/src/Containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <Sidebar />
+        </div>
+    );
+}
+
+export default App;
